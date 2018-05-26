5e65cd19f053e27ea36d8c9963c3a60c0f8ce26f
Mohit sahu <iamcool.909@gmail.com>
2018-05-26 11:18:49 +0530

    company page added

--- public/scripts/controllers/loginController.js
+++ public/scripts/controllers/loginController.js
@@ -4,8 +4,7 @@ tabtalent.controller('LoginController', ['$http', '$scope', '$stateParams', '$st
 
         $http.post('/users/login', $scope.cred).then(function (res) {
             $rootScope.user = res.data.user;
-            console.log("\r\n\n\n  $rootScope.user ",  $rootScope.user)
-            $state.go('app.profile');
+            $state.go('company');
         }, function (error) {
             $scope.message = error.data.message;
         });
